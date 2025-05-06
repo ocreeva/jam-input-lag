57ca30aeddac9551b3b8303732aa503dba9d2266
Brian Perrin <perrin@live.com>
2025-05-06 11:33:47 -0700

    Add Require attribute and property drawer.

---
+++ Assets/Editor/Extensions/VisualElementExtensions.cs
@@ -0,0 +1,8 @@
+namespace UnityEngine.UIElements
+{
+    internal static class VisualElementExtensions
+    {
+        public static void Align(this VisualElement element)
+        => element.AddToClassList("unity-base-field__aligned");
+    }
+}
