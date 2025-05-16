115ff10934e9fd26c4bc13d717a262d0c89b7e95
Brian Perrin <perrin@live.com>
2025-05-16 11:04:02 -0700

    Remove Z-axis from Coordinate / CoordinateOffset.

--- Assets/Core/Scripts/Coordinate.cs
+++ Assets/Core/Scripts/Coordinate.cs
@@ -6,39 +6,38 @@ namespace Moyba
     [Serializable]
     public struct Coordinate : IEquatable<Coordinate>
     {
-        private const float _ScaleH = 2.0f;
-        private const float _ScaleV = 1.0f;
+        private const float _Scale = 2.0f;
 
-        public int x, y, z;
+        public int x, y;
 
-        public Coordinate(int x, int y, int z) => (this.x, this.y, this.z) = (x, y, z);
+        public Coordinate(int x, int y) => (this.x, this.y) = (x, y);
         public Coordinate(Vector3 worldPosition) : this(
-            Mathf.RoundToInt(worldPosition.x / _ScaleH),
-            Mathf.RoundToInt(worldPosition.z / _ScaleH),
-            Mathf.RoundToInt(worldPosition.y / _ScaleV)
-        )
-        { }
+            Mathf.RoundToInt(worldPosition.x / _Scale),
+            Mathf.RoundToInt(worldPosition.z / _Scale)) { }
 
         public static bool operator ==(Coordinate c1, Coordinate c2)
-        => (c1.x == c2.x) && (c1.y == c2.y) && (c1.z == c2.z);
+        => (c1.x == c2.x) && (c1.y == c2.y);
 
         public static bool operator !=(Coordinate c1, Coordinate c2)
-        => (c1.x != c2.x) || (c1.y != c2.y) || (c1.z != c2.z);
+        => (c1.x != c2.x) || (c1.y != c2.y);
 
         public static Coordinate operator +(Coordinate c, CoordinateOffset o)
-        => new Coordinate(c.x + o.x, c.y + o.y, c.z + o.z);
+        => new Coordinate(c.x + o.x, c.y + o.y);
 
         public static Coordinate operator -(Coordinate c, CoordinateOffset o)
-        => new Coordinate(c.x - o.x, c.y - o.y, c.z - o.z);
+        => new Coordinate(c.x - o.x, c.y - o.y);
 
         public bool Equals(Coordinate other) => this == other;
         public override bool Equals(object obj)
         => obj is Coordinate other && this.Equals(other);
 
         public override int GetHashCode()
-        => HashCode.Combine(this.x, this.y, this.z);
+        => HashCode.Combine(this.x, this.y);
+
+        public override string ToString()
+        => $"{this.x:##0;-##0}, {this.y:##0,-##0}";
 
         public Vector3 ToWorldPosition()
-        => new Vector3(this.x * _ScaleH, this.z * _ScaleV, this.y * _ScaleH);
+        => new Vector3(this.x * _Scale, 0f, this.y * _Scale);
     }
 }
--- Assets/Core/Scripts/CoordinateOffset.cs
+++ Assets/Core/Scripts/CoordinateOffset.cs
@@ -5,35 +5,35 @@ namespace Moyba
     [Serializable]
     public struct CoordinateOffset : IEquatable<CoordinateOffset>
     {
-        public int x, y, z;
+        public int x, y;
 
-        public CoordinateOffset(int x, int y, int z = 0) => (this.x, this.y, this.z) = (x, y, z);
+        public CoordinateOffset(int x, int y) => (this.x, this.y) = (x, y);
 
-        public CoordinateOffset right => new CoordinateOffset(this.y, -this.x, this.z);
+        public CoordinateOffset right => new CoordinateOffset(this.y, -this.x);
 
         public static bool operator ==(CoordinateOffset o1, CoordinateOffset o2)
-        => (o1.x == o2.x) && (o1.y == o2.y) && (o1.z == o2.z);
+        => (o1.x == o2.x) && (o1.y == o2.y);
 
         public static bool operator !=(CoordinateOffset o1, CoordinateOffset o2)
-        => (o1.x != o2.x) || (o1.y != o2.y) || (o1.z != o2.z);
+        => (o1.x != o2.x) || (o1.y != o2.y);
 
         public static CoordinateOffset operator +(CoordinateOffset o1, CoordinateOffset o2)
-        => new CoordinateOffset(o1.x + o2.x, o1.y + o2.y, o1.z + o2.z);
+        => new CoordinateOffset(o1.x + o2.x, o1.y + o2.y);
 
         public static CoordinateOffset operator -(CoordinateOffset o1, CoordinateOffset o2)
-        => new CoordinateOffset(o1.x - o2.x, o1.y - o2.y, o1.z - o2.z);
+        => new CoordinateOffset(o1.x - o2.x, o1.y - o2.y);
 
         public static CoordinateOffset operator *(CoordinateOffset o, int m)
-        => new CoordinateOffset(o.x * m, o.y * m, o.z * m);
+        => new CoordinateOffset(o.x * m, o.y * m);
 
         public bool Equals(CoordinateOffset other) => this == other;
         public override bool Equals(object obj)
         => obj is CoordinateOffset other && this.Equals(other);
 
         public override int GetHashCode()
-        => HashCode.Combine(this.x, this.y, this.z);
+        => HashCode.Combine(this.x, this.y);
 
         public override string ToString()
-        => $"{this.x:+##0;-##0}, {this.y:+##0,-##0}, {this.z:+##0,-##0}";
+        => $"{this.x:+##0;-##0}, {this.y:+##0,-##0}";
     }
 }
