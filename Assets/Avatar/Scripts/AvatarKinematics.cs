a6fe00fa6eef018554e2b1358797cd5c4a05aa24
Brian Perrin <perrin@live.com>
2025-05-17 16:14:47 -0700

    More Avatar Kinematics Implement turning. Implement strafing. Update animation for strafing.

--- Assets/Avatar/Scripts/AvatarKinematics.cs
+++ Assets/Avatar/Scripts/AvatarKinematics.cs
@@ -10,6 +10,11 @@ namespace Moyba.Avatar
         private const string _AvatarSpeedLiteral = "Speed";
         private static readonly int _AvatarSpeed = Animator.StringToHash(_AvatarSpeedLiteral);
 
+        private const string _AvatarStrafeLiteral = "Strafe";
+        private static readonly int _AvatarStrafe = Animator.StringToHash(_AvatarStrafeLiteral);
+
+        private static readonly Quaternion _Right = Quaternion.Euler(0, 90, 0);
+
         private static readonly _StubAvatarKinematics _Stub = new _StubAvatarKinematics();
 
         internal static IAvatarKinematics Stub => _Stub;
@@ -24,11 +29,18 @@ namespace Moyba.Avatar
         [SerializeField, Range(-10f, 10f)] private float _initialSpeed = 0f;
         [SerializeField, Range(-10f, 0f)] private float _minSpeed = -0.5f;
         [SerializeField, Range(0f, 10f)] private float _maxSpeed = 5f;
+        [SerializeField, Range(0f, 90f)] private float _strafeAngle = 45f;
+        [SerializeField, Range(0f, 10f)] private float _turnRate = 1.2f;
 
         [NonSerialized] private Rigidbody _rigidbody;
 
         [NonSerialized] private float _speed;
-        [NonSerialized] private int _speedDelta;
+        [NonSerialized] private int _speedInput;
+
+        [NonSerialized] private int _strafeInput;
+
+        [NonSerialized] private Quaternion _facing = Quaternion.identity;
+        [NonSerialized] private int _turnInput;
 
         private void Awake()
         {
@@ -39,6 +51,7 @@ namespace Moyba.Avatar
             _rigidbody = this.GetComponent<Rigidbody>();
 
             _speed = _initialSpeed;
+            _facing = this.transform.rotation;
         }
 
 #if UNITY_EDITOR
@@ -48,8 +61,12 @@ namespace Moyba.Avatar
         }
 #endif
 
-        private void HandleSpeedChanged(int speedDelta)
-        => _speedDelta = speedDelta;
+        private void HandleSpeedChanged(int speedInput)
+        => _speedInput = speedInput;
+        private void HandleStrafeChanged(int strafeInput)
+        => _strafeInput = strafeInput;
+        private void HandleTurnChanged(int turnInput)
+        => _turnInput = turnInput;
 
         private void OnDestroy()
         {
@@ -61,6 +78,8 @@ namespace Moyba.Avatar
         private void OnDisable()
         {
             Omnibus.Input.Avatar.OnSpeedChanged -= this.HandleSpeedChanged;
+            Omnibus.Input.Avatar.OnStrafeChanged -= this.HandleStrafeChanged;
+            Omnibus.Input.Avatar.OnTurnChanged -= this.HandleTurnChanged;
 
 #if UNITY_EDITOR
             Omnibus.Input.Debug.OnSignal -= this.HandleDebugSignal;
@@ -70,6 +89,8 @@ namespace Moyba.Avatar
         private void OnEnable()
         {
             Omnibus.Input.Avatar.OnSpeedChanged += this.HandleSpeedChanged;
+            Omnibus.Input.Avatar.OnStrafeChanged += this.HandleStrafeChanged;
+            Omnibus.Input.Avatar.OnTurnChanged += this.HandleTurnChanged;
 
 #if UNITY_EDITOR
             Omnibus.Input.Debug.OnSignal += this.HandleDebugSignal;
@@ -78,23 +99,36 @@ namespace Moyba.Avatar
 
         private void FixedUpdate()
         {
-            if (_speedDelta != 0)
+            if (_speedInput != 0)
             {
                 var rate = _speed > 0
-                    ? (_speedDelta > 0 ? _positiveAcceleration : _deceleration)
-                    : (_speedDelta > 0 ? _deceleration : _negativeAcceleration);
-                _speed += _speedDelta * rate * Time.deltaTime;
+                    ? (_speedInput > 0 ? _positiveAcceleration : _deceleration)
+                    : (_speedInput > 0 ? _deceleration : _negativeAcceleration);
+                _speed += _speedInput * rate * Time.deltaTime;
                 _speed = Mathf.Clamp(_speed, _minSpeed, _maxSpeed);
             }
 
-            // TODO: turn
+            if (_turnInput != 0)
+            {
+                _facing = Quaternion.LerpUnclamped(_facing, _facing * _Right, _turnInput * _turnRate * Time.deltaTime);
+            }
+
+            var velocity = _facing * Vector3.forward * _speed;
+            if (_strafeInput != 0)
+            {
+                velocity = Quaternion.Euler(0, _strafeAngle * _strafeInput * Mathf.Sign(_speed), 0) * velocity;
+            }
+
             // TODO: in air
-            _rigidbody.linearVelocity = new Vector3(0, 0, _speed);
+            _rigidbody.linearVelocity = velocity;
         }
 
         private void Update()
         {
             _animator.SetFloat(_AvatarSpeed, _speed);
+            _animator.SetFloat(_AvatarStrafe, _speed * _strafeInput);
+
+            this.transform.rotation = _facing;
         }
 
         private class _StubAvatarKinematics : ATraitStub<AvatarKinematics>, IAvatarKinematics { }
